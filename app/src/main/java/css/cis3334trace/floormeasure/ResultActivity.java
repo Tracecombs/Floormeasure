ad72477cf78eacc7910d7c00ba07b6f49724df8e
tcombs1 <tcombs1@css.edu>
2017-04-23 17:26:59 -0500

    Fixed the issues with converting strings to doubles, getting the "R" error that seems to appear after using the emulator

--- app/src/main/java/css/cis3334trace/floormeasure/ResultActivity.java
+++ app/src/main/java/css/cis3334trace/floormeasure/ResultActivity.java
@@ -1,6 +1,7 @@
 package css.cis3334trace.floormeasure;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -9,8 +10,7 @@ import android.widget.TextView;
 public class ResultActivity extends AppCompatActivity {
 
 
-    String Width;
-    String Length;
+   
     TextView tvResult;
 
     @Override
@@ -19,16 +19,16 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
         tvResult = (TextView)  findViewById(R.id.textViewResult);
 
-        Bundle extras = getIntent().getExtras();
+        Intent intent = getIntent();
+        String width = intent.getExtras().getString("Width");
+        String length = intent.getExtras().getString("Length");
 
-        Width = extras.getSerializable("Width").toString();
-        Length = extras.getSerializable("Length").toString();
 
 
-        double Width2 = Double.parseDouble("Width");
-        double Length2 = Double.parseDouble("Length");
+        double Width2 = Double.parseDouble(width);
+        double Length2 = Double.parseDouble(length);
         double total = Width2 * Length2;
-        tvResult.setText("with is " + Width2 + " Length is " + Length2 + " your total flooring needed is " + total);
+        tvResult.setText("width is " + Width2 + " Length is " + Length2 + " your total flooring needed is " + total);
     }
 
 };
